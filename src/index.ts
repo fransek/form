8861b76e31e34b45aaf98dcc91052aea2688a8f1
Frans Ekman <fransedvinekman@gmail.com>
2026-06-16 21:50:25 +0200

    fix: exposed `SubmitContext` `CommitOptions` types to public API

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ export { Form } from "./lib/form";
 export { createFieldState, validate, validateAsync } from "./lib/state-utils";
 export type {
   AsyncValidator,
+  CommitOptions,
   FieldProps,
   FieldRenderProps,
   FieldState,
   FormProps,
+  SubmitContext,
   SyncValidator,
   Validation,
   ValidationMode,
